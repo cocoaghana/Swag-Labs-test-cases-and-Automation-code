04f8faad5bee55a6cf8339a0ad250e7ef5d6d00e
cocoaghana <phitchayapha.vitta@gmail.com>
2026-06-20 15:59:25 +0700

    contact test case automation
    contact test case automation
    format test cases excel

--- Cypress Automation/cypress/e2e/Contact.cy.js
+++ Cypress Automation/cypress/e2e/Contact.cy.js
@@ -1,9 +1,16 @@
 describe('Contact Function', () => {
     beforeEach(() => {
-        cy.login()
-        cy.visit('https://www.saucedemo.com/inventory.html',{ failOnStatusCode: false }) 
+        cy.visit('https://www.saucedemo.com/') 
+        cy.setCookie('session-username', 'standard_user')
+        cy.visit('https://www.saucedemo.com/inventory.html', { failOnStatusCode: false }) 
     })
     it('TCID_007 X contact', () => {
-    
-  })
+      cy.get('[data-test="social-twitter"]').should('have.attr', 'href', 'https://twitter.com/saucelabs')
+    })
+    it('TCID_008 Facebook contact', () => {
+      cy.get('[data-test="social-facebook"]').should('have.attr', 'href', 'https://www.facebook.com/saucelabs')
+    })
+    it('TCID_009 LinkedIn contact', () => {
+      cy.get('[data-test="social-linkedin"]').should('have.attr', 'href', 'https://www.linkedin.com/company/sauce-labs/')
+    })
 })
